746d31df69ef4501b4b5f826dccc5762ea3897dd
SaltySalt77 <hyna0707@gmail.com>
2024-12-28 06:58:14 +0900

    🐛 Fix : PWA manifest 적용 안되던 문제 해결

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body>{children}</body>
     </html>
   );
