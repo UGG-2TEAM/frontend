c969ed4c3f1e66fefcc82926b1506ba931696207
SaltySalt77 <hyna0707@gmail.com>
2024-12-28 07:59:37 +0900

    Fix : 빌드 에러

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,5 @@
 /** @type {import('next').NextConfig} */
-import withPWA from "next-pwa";
 
 const nextConfig = {};
 
-export default withPWA({
-  ...nextConfig,
-  pwa: {
-    dest: "public", // manifest.json과 서비스 워커 파일 위치
-    register: true,
-    skipWaiting: true,
-  },
-});
+export default nextConfig;
